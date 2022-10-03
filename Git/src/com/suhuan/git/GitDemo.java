9dc1ed2e84b416d0a9ed495810b60ea176a79e7d
suhuan <1052417877@qq.com>
2022-10-03 23:26:00 +0800

    third commit

---
+++ Git/src/com/suhuan/git/GitDemo.java
@@ -0,0 +1,13 @@
+package com.suhuan.git;
+
+/**
+ * @Auther: suhuan
+ * @Date: 2022/10/3 - 10 - 03 - 23:19
+ */
+public class GitDemo {
+
+    public static void main(String[] args) {
+        System.out.println("hi~");
+    }
+
+}
