7ed785c241b63bdd5817ae46e6599b1baf540d1e
suhuan <1052417877@qq.com>
2022-10-04 13:16:05 +0800

    hot-fix commit1

--- Git/src/com/suhuan/git/GitDemo.java
+++ Git/src/com/suhuan/git/GitDemo.java
@@ -8,6 +8,8 @@ public class GitDemo {
 
     public static void main(String[] args) {
         System.out.println("hi~");
+        System.out.println("hi~1");
+        System.out.println("hi~2");
     }
 
 }
