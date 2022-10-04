717a4eec74139da7c2a13f1522020097458c626b
suhuan <1052417877@qq.com>
2022-10-04 14:20:36 +0800

    push commit

---
+++ Git/src/com/suhuan/git/GitPushAddDemo.java
@@ -0,0 +1,13 @@
+package com.suhuan.git;
+
+/**
+ * @Auther: suhuan
+ * @Date: 2022/10/4 - 10 - 04 - 14:18
+ */
+public class GitPushAddDemo {
+
+    public static void main(String[] args) {
+        System.out.println("push1");
+    }
+
+}
